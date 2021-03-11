f5738b065a5e06777b7ffcc7bdb34050df368610
Nicolas Mariño <nicolasmarinoparra@gmail.com>
2021-03-10 23:42:16 -0500

    fix problem in api call

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -2,7 +2,7 @@ require 'json'
 class VideosController < ApplicationController
   def index
     @response = JSON.parse(ZypeAPI::Videos.show_all)["response"]
-    p JSON.parse(ZypeAPI::Videos.show_all.body)["response"]
+    p JSON.parse(ZypeAPI::Videos.show_all)["response"]
   end
 
   def show
