43c8ee626ee4a83f5dfd1226992c3911b8a95484
Nicolas Mariño <nicolasmarinoparra@gmail.com>
2021-03-10 23:34:07 -0500

    add debug message for heroku deploy

--- app/services/ZypeAPI.rb
+++ app/services/ZypeAPI.rb
@@ -5,7 +5,7 @@ require 'openssl'
 module ZypeAPI
     class Videos
       def self.show_all
-        url = URI("https://api.zype.com/videos?per_page=500&sort=published_at&app_key=#{ENV['APP_KEY']}")
+        url = URI("https://api.zype.com/videos?per_page=500&sort=published_at&app_key=XWny5j0V89yb1uZu6SI_D95EADV5FzBYldE9Ny0_q0GOzcWLiruPyhN-f2Pcyohf")
         http = Net::HTTP.new(url.host, url.port)
         http.use_ssl = true
 
@@ -21,7 +21,7 @@ module ZypeAPI
       end
 
       def self.authenticate(username, password)
-        url = URI("https://login.zype.com/oauth/token?client_id=#{ENV['CLIENT_ID']}&client_secret=#{ENV['CLIENT_SECRET']}&username=#{username}&password=#{password}&grant_type=password")
+        url = URI("https://login.zype.com/oauth/token?client_id=61255480307354ebd4d094482f2483adec9942637979aa5c3963ecbac469f943&client_secret=926e632b2aa9758f60dbdf2f8de13bebd9a04dfd602de4c257b3f8b4a97cf0b8&username=#{username}&password=#{password}&grant_type=password")
         http = Net::HTTP.new(url.host, url.port)
         http.use_ssl = true
 
