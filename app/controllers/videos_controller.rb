83b7f90149aa98aa6b205478b81b338d7ee3958a
Nicolas Mariño <nicolasmarinoparra@gmail.com>
2021-03-10 23:09:20 -0500

    add debug message for heroku deploy

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -2,6 +2,7 @@ require 'json'
 class VideosController < ApplicationController
   def index
     @response = JSON.parse(ZypeAPI::Videos.show_all.body)["response"]
+    p JSON.parse(ZypeAPI::Videos.show_all.body)["response"]
   end
 
   def show
